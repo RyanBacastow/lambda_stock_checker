743902a7b34cdae86cc11abd274e6b3ec02b2ece
ryan.bacastow <ryan.bacastow@tmsw.com>
2019-12-02 03:31:28 -0600

    changed messaging, sns working.

--- handler.py
+++ handler.py
@@ -4,7 +4,10 @@ import json
 import boto3
 from datetime import datetime
 from os import environ as env
+import logging
 
+boto3.set_stream_logger('boto3.resources', logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 def lambda_handler(event, context):
     try:
@@ -14,7 +17,7 @@ def lambda_handler(event, context):
             json_data = responseData.read()
             deserialised_data = json.loads(json_data)
             print(deserialised_data)
-        final_string = """"""
+        final_string = """Checking indexes at {tz_datetime} UTC: \n""".format(tz_datetime=datetime.utcnow())
         market_indicator_total=0.0
         for ticker in deserialised_data['majorIndexesList']:
                 ticker_name = ticker['ticker']
@@ -23,19 +26,17 @@ def lambda_handler(event, context):
                 change_float = float(price_change)
                 market_indicator_total+=change_float
                 if change_float > 0:
-                    price_change_type = 'positive'
+                    price_change_type = 'upward'
                 elif change_float < 0:
-                    price_change_type = 'negative'
+                    price_change_type = 'downward'
                 else:
                     price_change_type = 'neutral'
 
-
-                final_string += "The {full_ticker_name} index (ticker:{ticker_name})has had a {price_change_type} price change of {price_change} since yesterday's price as of {tz_datetime} UTC.\n"\
+                final_string += "The {full_ticker_name} index (ticker:{ticker_name}) trended {price_change_type} {price_change} at last close.\n"\
                                   .format(full_ticker_name=full_ticker_name,
                                           ticker_name=ticker_name,
                                           price_change_type=price_change_type,
-                                          price_change=price_change,
-                                          tz_datetime=datetime.utcnow()
+                                          price_change=price_change
                                           )
 
 
@@ -48,15 +49,17 @@ def lambda_handler(event, context):
 
 
 def publish_message_sns(message):
-    sns_arn = env.get('SNS_ARN')
+    sns_arn = env.get('SNS_ARN').strip()
     sns_client = boto3.client('sns')
     try:
         print(message)
 
         response = sns_client.publish(
-            TargetArn=sns_arn,
+            TopicArn=sns_arn,
             Message=message
         )
+
+        print(response)
 
     except Exception as e:
         print("ERROR PUBLISHING MESSAGE TO SNS: {}".format(e))
